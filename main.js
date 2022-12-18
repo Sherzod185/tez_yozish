f6d94f2bdded27b66c7355360ef93a833dcc0a70
sherzodboboxonov3 <sherzodboboxonov3@gmail.com>
2022-12-19 01:57:51 +0500

    ksjd

--- main.js
+++ main.js
@@ -167,7 +167,10 @@ elinput.addEventListener("input", e=>{
     yes.innerHTML="✅"
     e.target.value = "";
   }
-  else{
+  else if((typedText.length === randomword.length)){
     yes.innerHTML="❌"
   }
+	else{
+		yes.innerHTML=""
+	}
 });
